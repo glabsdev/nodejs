3be5c90e84abd8c10cd6f00ba0c2cefc065de233
glabsdev <53467246+glabsdev@users.noreply.github.com>
2019-08-08 10:59:25 +0800

    Add files via upload

--- sms-mt.js
+++ sms-mt.js
@@ -1,16 +1,22 @@
 var request = require("request");
 
+var shortcode = '2648';
+var access_token = 'A-2szoYus7mB13l5axDrr_1234AApSz8eu236GRNsoBQ';
+var address = '9771234567';
+var clientCorrelator = '264801';
+var message = 'NodeJS SMS Test';
+
 var options = { method: 'POST',
-  url: 'https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/<Shortcode>/requests',
-  qs: { access_token: '<access_token>' },
+  url: 'https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/' + shortcode + '/requests',
+  qs: { 'access_token': access_token },
   headers: 
-   {'Content-Type': 'application/json' },
+   { 'Content-Type': 'application/json' },
   body: 
-   { outboundSMSMessageRequest: 
-      { clientCorrelator: '<clientCorrelator>',
-        senderAddress: '<Shortcode>',
-        outboundSMSTextMessage: { message: 'Hello World' },
-        address: '<address>' } },
+   { 'outboundSMSMessageRequest': 
+      { 'clientCorrelator': clientCorrelator,
+        'senderAddress': shortcode,
+        'outboundSMSTextMessage': { 'message': message },
+        'address': address } },
   json: true };
 
 request(options, function (error, response, body) {
