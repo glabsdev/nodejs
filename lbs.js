f4107ee0ae9ab50e974c6f1888ed23f2d6d93117
glabsdev <53467246+glabsdev@users.noreply.github.com>
2019-08-08 12:26:21 +0800

    Add files via upload

--- lbs.js
+++ lbs.js
@@ -1,10 +1,15 @@
 var request = require("request");
+
+var access_token = 'A-2szoYus7mB13l5axDrr_1234AApSz8eu236GRNsoBQ';
+var address = '9771234567';
+var requestedAccuracy = '100';
+
 var options = { method: 'GET',
   url: 'https://devapi.globelabs.com.ph/location/v1/queries/location',
   qs: 
-   { access_token: '<access_token>',
-     address: '<address>',
-     requestedAccuracy: '100' },
+   { 'access_token': access_token,
+     'address': address,
+     'requestedAccuracy': requestedAccuracy },
   headers: 
    { Host: 'devapi.globelabs.com.ph',
      'Content-Type': 'application/json' } };
