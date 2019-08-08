bf84234bf23cbb532e6b78e916901eb01243b354
glabsdev <53467246+glabsdev@users.noreply.github.com>
2019-08-08 15:55:57 +0800

    Add files via upload

--- load_api.js
+++ load_api.js
@@ -1,17 +1,26 @@
 var request = require("request");
+
+var app_id = 'B54z9Ug5512HHrTGRT5g6hq64pGUq6ap';
+var app_secret = 'f655413712345607a696cd40741993758c411af3bb5f6c230270ec26e8d54126';
+var rewards_token = 'AbCkxKYid_F_p-JSgTejow';
+var address = '9001234567';
+var promo = 'LOAD 1';
+
 var options = { method: 'POST',
   url: 'https://devapi.globelabs.com.ph/rewards/v1/transactions/send',
   headers: 
    { Host: 'devapi.globelabs.com.ph', 'Content-Type': 'application/json' },
   body: 
    { outboundRewardRequest: 
-      { app_id: '<app_id>',
-        app_secret: '<app_secret>',
-        rewards_token: '<rewards_token>',
-        address: '<address>',
-        promo: '<promo>' } },
+      { 'app_id': app_id,
+        'app_secret': app_secret,
+        'rewards_token': rewards_token,
+        'address': address,
+        'promo': promo } },
   json: true };
 request(options, function (error, response, body) {
   if (error) throw new Error(error);
   console.log(body);
 });
+
+
--- load_api_status.js
+++ load_api_status.js
@@ -1,14 +1,20 @@
 var request = require("request");
+
+var app_id = 'B54z9Ug5512HHrTGRT5g6hq64pGUq6ap';
+var app_secret = 'f655413712345607a696cd40741993758c411af3bb5f6c230270ec26e8d54126';
+var rewards_token = 'AbCkxKYid_F_p-JSgTejow';
+var transaction_id = '1234567';
+
 var options = { method: 'POST',
   url: 'https://devapi.globelabs.com.ph/rewards/v1/transactions/status',
   headers: 
    { 'Content-Type': 'application/json' },
   body: 
    { rewardStatusRequest: 
-      { app_id: '<app_id>',
-        app_secret: '<app_secret>',
-        rewards_token: '<rewards_token>',
-        transaction_id: '<transaction_id>' } },
+      { 'app_id': app_id,
+        'app_secret': app_secret,
+        'rewards_token': rewards_token,
+        'transaction_id': transaction_id } },
   json: true };
 request(options, function (error, response, body) {
   if (error) throw new Error(error);
